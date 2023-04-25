b062a6535e4012b810269e19144406a72817f0d7
aleksvvv <adm-shop-001@yandex.ru>
2023-04-25 09:21:15 +0300

    Scaffold, SnackBar, FAB, State

---
+++ app/src/main/java/com/bignerdranch/android/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -0,0 +1,71 @@
+package com.bignerdranch.android.vknewsclient.ui.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "Hello World",
+                        actionLabel = "Close",
+                        duration = SnackbarDuration.Long
+                    )
+                }
+
+            }) {
+                Icon(Icons.Default.Add, contentDescription = null)
+            }
+        },
+        bottomBar = {
+            BottomNavigation {
+                val selectedItemPosition = remember {
+                    mutableStateOf(0)
+                }
+
+                val items =
+                    arrayOf(
+                        NavigationItem.Home,
+                        NavigationItem.Favorite,
+                        NavigationItem.Profile
+                    )
+                items.forEachIndexed { index, item ->
+                    BottomNavigationItem(
+                        selected = selectedItemPosition.value == index,
+                        onClick = {
+                            selectedItemPosition.value = index
+                        },
+                        label = {
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        selectedContentColor = MaterialTheme.colors.onPrimary,
+                        unselectedContentColor = MaterialTheme.colors.onSecondary
+                    )
+                }
+            }
+        }
+    ) {
+
+    }
+}

19aed7e0e935de1810e8fed4e1edd318c71319df
aleksvvv <adm-shop-001@yandex.ru>
2023-05-01 16:35:35 +0300

    ScreenComments

--- app/src/main/java/com/bignerdranch/android/vknewsclient/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/bignerdranch/android/vknewsclient/ui/theme/HomeScreen.kt
@@ -12,64 +12,78 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bignerdranch.android.vknewsclient.MainViewModel
+import com.bignerdranch.android.vknewsclient.domain.PostComment
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     viewModel: MainViewModel,
-){
+) {
     val feedPosts =
         viewModel.feedPosts.observeAsState(listOf())
-
-    LazyColumn(
-        contentPadding = PaddingValues(
-            top = 16.dp,
-            start = 8.dp,
-            end = 8.dp,
-            bottom = 72.dp
-        ),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-        items(
-            items = feedPosts.value,
-            key = { it.id }
-        ) { feedPost ->
-            val stateDismiss = rememberDismissState()
-            if (stateDismiss.isDismissed(DismissDirection.EndToStart)){
-                viewModel.remove(feedPost)
+    if (feedPosts.value.isNotEmpty()) {
+        val comments = mutableListOf<PostComment>().apply {
+            repeat(20) {
+                add(PostComment(id = it))
             }
-            SwipeToDismiss(
-                modifier = Modifier.animateItemPlacement(),
-                state = stateDismiss,
-                directions = setOf(DismissDirection.EndToStart),
-                background = {
-                    Box(
-                        modifier = Modifier.padding(12.dp)
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.CenterEnd) {
-                        Text(text = "Delete Item",
-                            fontSize = 24.sp)
-                    }
-
-                }) {
-                PostVcCard(
-                    feedPost = feedPost,
-                    onViewClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onSharesClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onCommentsClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    },
-                    onLikesClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    }
-                )
-            }
-
         }
+
+        CommentsScreen(
+            feedPost = feedPosts.value.get(0),
+            comments
+        )
     }
+
+
+//    LazyColumn(
+//        contentPadding = PaddingValues(
+//            top = 16.dp,
+//            start = 8.dp,
+//            end = 8.dp,
+//            bottom = 72.dp
+//        ),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//    ) {
+//
+//        items(
+//            items = feedPosts.value,
+//            key = { it.id }
+//        ) { feedPost ->
+//            val stateDismiss = rememberDismissState()
+//            if (stateDismiss.isDismissed(DismissDirection.EndToStart)){
+//                viewModel.remove(feedPost)
+//            }
+//            SwipeToDismiss(
+//                modifier = Modifier.animateItemPlacement(),
+//                state = stateDismiss,
+//                directions = setOf(DismissDirection.EndToStart),
+//                background = {
+//                    Box(
+//                        modifier = Modifier.padding(12.dp)
+//                            .fillMaxSize(),
+//                        contentAlignment = Alignment.CenterEnd) {
+//                        Text(text = "Delete Item",
+//                            fontSize = 24.sp)
+//                    }
+//
+//                }) {
+//                PostVcCard(
+//                    feedPost = feedPost,
+//                    onViewClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onSharesClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onCommentsClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    },
+//                    onLikesClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    }
+//                )
+//            }
+//
+//        }
+//    }
 }
